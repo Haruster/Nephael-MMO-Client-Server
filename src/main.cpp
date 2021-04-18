5e29de920b09cdce9b95745b215adf2457ba5f02
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-19 05:05:30 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,16 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+#ifdef _WIN32
+
+#define _WIN32_WINNT 0x0A00
+
+#endif
+
 #define ASIO_STANDALONE
 
 int main() {
+ 
     return 0;
+
 }
